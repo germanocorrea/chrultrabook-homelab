527d7db65f23cb72fcc060b7866909bb15db0f6f
Germano Corrêa <germanobruscato@gmail.com>
2026-02-05 23:24:20 -0300

    added trusted user

--- hosts/chrultrabook-homelab/configuration.nix
+++ hosts/chrultrabook-homelab/configuration.nix
@@ -97,5 +97,5 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "25.11"; # Did you read the comment?
-
+  nix.settings.trusted-users = [ "root" "gege" ];
 }
