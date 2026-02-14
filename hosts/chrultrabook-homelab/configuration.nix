ead52b5fc8a8827592139e87089e138d9dd09848
Germano Corrêa <germanobruscato@gmail.com>
2026-02-14 18:55:10 -0300

    new chrultrabook configuration

--- hosts/chrultrabook-homelab/configuration.nix
+++ hosts/chrultrabook-homelab/configuration.nix
@@ -1,33 +1,24 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
-{ config, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 {
   imports = [
-    # Include the results of the hardware scan.
     ./hardware-configuration.nix
   ];
 
-  # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.hostName = "chrultrabook-homelab";
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
   networking.networkmanager.enable = true;
 
-  # Set your time zone.
   time.timeZone = "America/Sao_Paulo";
 
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
 
   i18n.extraLocaleSettings = {
@@ -46,54 +37,26 @@
   # console.keyMap = "br-abnt2";
   console.keyMap = "us";
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.gege = {
     isNormalUser = true;
+    home = "/home/gege";
     description = "gege";
     extraGroups = [
-      "networkmanager"
       "wheel"
+      "gege"
     ];
-    packages = with pkgs; [ ];
   };
 
-  # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    git
+    vim
     wget
+    git
   ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
   services.openssh.enable = true;
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "25.11"; # Did you read the comment?
+  system.stateVersion = "25.11";
   nix.settings.trusted-users = [
     "root"
     "gege"
@@ -109,12 +72,10 @@
   };
   services.homelab = {
     storage = "/mnt/Storage";
-    # Define o caminho do socket para o módulo de containers
     brokerbotSocket = "/run/user/1000/brokerbot";
 
   };
 
-  # Cria o diretório do socket no host com as permissões corretas
   systemd.tmpfiles.rules = [
     "d /run/user/1000/brokerbot 0755 gege users - -"
   ];
