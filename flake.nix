14e554a3c7b89ee395b1eb298f82afbeb59fab59
Germano Bruscato Corrêa <germanobruscato@gmail.com>
2026-05-09 01:58:42 -0300

    initial sops secrets management

--- flake.nix
+++ flake.nix
@@ -3,10 +3,14 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
-    { self, nixpkgs }:
+    { self, nixpkgs, sops-nix }:
     {
 
       packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
@@ -19,6 +23,7 @@
           modules = [
             ./hosts/chrultrabook-homelab/configuration.nix
             ./modules/homelab/default-configuration.nix
+            sops-nix.nixosModules.sops
           ];
         };
         vm-homelab = nixpkgs.lib.nixosSystem {
@@ -26,6 +31,7 @@
           modules = [
             ./hosts/vm-homelab/configuration.nix
             ./modules/homelab/default-configuration.nix
+            sops-nix.nixosModules.sops
           ];
         };
       };
