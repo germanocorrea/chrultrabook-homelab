30c134fd262fcbc250b78efe64b1c4e8a39160eb
Germano Bruscato Corrêa <germanobruscato@gmail.com>
2026-07-19 20:40:21 -0300

    orgmode: timer based updates on org-roam and ical files

--- modules/homelab/orgmode.nix
+++ modules/homelab/orgmode.nix
@@ -1,8 +1,31 @@
-{ config, pkgs, doomConfigDir, ... }:
+{ config, lib, pkgs, doomConfigDir, ... }:
 let
   user = "gege";
+
+  timerBasedAction = parameters: {
+    systemd.user.services.${parameters.slug} = {
+      description = parameters.slug;
+      after = [ "emacs.service" ];
+      wants = [ "emacs.service" ];
+      serviceConfig = {
+        Type = "oneshot";
+        ExecStart = "${pkgs.emacs}/bin/emacsclient --eval '(${parameters.command})'";
+      };
+    };
+
+    systemd.user.timers.${parameters.slug} = {
+      description = "Run ${parameters.slug} periodically";
+      timerConfig = {
+        OnBootSec = "30s";
+        OnUnitActiveSec = "30s";
+        Persistent = true;
+      };
+      wantedBy = [ "timers.target" ];
+    };
+  };
 in
-{
+lib.mkMerge [
+  {
     services.syncthing = {
       enable = true;
       openDefaultPorts = true;
@@ -12,36 +35,42 @@ in
       };
     };
 
-  networking.firewall.allowedTCPPorts = [ 8384 ];
+    networking.firewall.allowedTCPPorts = [ 8384 ];
 
-  environment.systemPackages = with pkgs; [
-    syncthing
-    emacs
-  ];
+    environment.systemPackages = with pkgs; [
+      syncthing
+      emacs
+    ];
 
-  services.emacs.enable = true;
+    services.emacs.enable = true;
 
-  system.activationScripts.doomSync = {
-    text = ''
-      ${pkgs.util-linux}/bin/runuser - ${toString user} -c '
-        set -e
+    system.activationScripts.doomSync = {
+      text = ''
+        ${pkgs.util-linux}/bin/runuser - ${user} -c '
+          set -e
 
-        if [ ! -d /home/${toString user}/.config/emacs ]; then
-          ${pkgs.git}/bin/git clone --depth 1 https://github.com/doomemacs/doomemacs \
-            /home/${toString user}/.config/emacs
-        fi
+          if [ ! -d /home/${user}/.config/emacs ]; then
+            ${pkgs.git}/bin/git clone --depth 1 https://github.com/doomemacs/doomemacs \
+              /home/${user}/.config/emacs
+          fi
 
-        mkdir -p /home/${toString user}/.config/doom
-        cp -rL --no-preserve=mode ${toString doomConfigDir}/. /home/${toString user}/.config/doom/
-        chmod -R u+w /home/${toString user}/.config/doom
+          mkdir -p /home/${user}/.config/doom
+          cp -rL --no-preserve=mode ${doomConfigDir}/. /home/${user}/.config/doom/
+          chmod -R u+w /home/${user}/.config/doom
 
-        if [ ! -d /home/${toString user}/.config/emacs/.local ]; then
-          /home/${toString user}/.config/emacs/bin/doom install --force
-        else
-          /home/${toString user}/.config/emacs/bin/doom sync
-        fi
-      '
-    '';
-    deps = [];
-  };
-}
+          if [ ! -d /home/${user}/.config/emacs/.local ]; then
+            /home/${user}/.config/emacs/bin/doom install --force
+          else
+            /home/${user}/.config/emacs/bin/doom sync
+          fi
+        '
+      '';
+      deps = [];
+    };
+  }
+
+  (lib.mkMerge (map timerBasedAction [
+    { slug = "org-roam-sync"; command = "org-roam-db-sync"; }
+    { slug = "org-calendar-sync"; command = "org-icalendar-combine-agenda-files"; }
+  ]))
+]
